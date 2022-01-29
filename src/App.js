02e97c82d3963a342c326c1e7a2181123b17e691
MaciejMondalski <maciej.mondalski@gmail.com>
2022-01-29 23:34:35 +0100

    new img sources

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Popup from './components/Popup';
 import { useState, useEffect, useCallback } from 'react';
 
 const cardImages = [
-  { src: '/img/helmet-1.png', matched: false },
-  { src: '/img/potion-1.png', matched: false },
-  { src: '/img/ring-1.png', matched: false },
-  { src: '/img/scroll-1.png', matched: false },
-  { src: '/img/shield-1.png', matched: false },
-  { src: '/img/sword-1.png', matched: false },
+  { src: '/img/burger.jpg', matched: false },
+  { src: '/img/hotdog.jpg', matched: false },
+  { src: '/img/donut.jpg', matched: false },
+  { src: '/img/sushi.jpg', matched: false },
+  { src: '/img/pizza.jpg', matched: false },
+  { src: '/img/durum.jpg', matched: false },
 ];
 
 function App() {
